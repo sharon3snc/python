b61553facca2147c96b4f08af53c23f41646be86
sharon3snc <sharonnunez89@gmail.com>
2023-02-15 21:19:25 +0100

    ok

---
+++ clase4_ejercicio1.py
@@ -0,0 +1,25 @@
+
+tiempo = {}
+
+for i in range(0, len(datos_tiempo['time'])):
+    tiempo[datos_tiempo['time'][i]] = {
+
+    }
+
+    has_maxTemp=False
+    has_minTemp=False
+
+    for key, value in datos_tiempo.items():
+        tiempo[datos_tiempo['time'][i]] [key]= value [i]
+
+    else 
+
+for fecha, info in tiempo: 
+    if "temperature_2m_max" in info and "temperature_2m_min"in info :
+        info[]
+
+import json
+print(json.dumps(tiempo, indent=2))
+
+
+
